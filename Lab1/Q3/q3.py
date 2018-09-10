1d4ebed39f43c5db65f8e58d9d355990b33c61c8
Luiz Felipe Schiaveto <luizfschiaveto@gmail.com>
2018-09-10 18:06:47 -0300

    Add 2nd question and alterations in 3rd question

--- Lab1/Q3/q3.py
+++ Lab1/Q3/q3.py
@@ -1,3 +1,6 @@
+import q1
+import q2
+
 def transition(states, symbol, graph):
     deathState = 10
     lenght = len(states)
@@ -41,9 +44,34 @@ if __name__ == "__main__":
     c= 'c'
     word1 = [b, a, a, b, b, a]
     word2 = [a, b, a, c, a, b, c]
-    finalStages = [0, 1, 2]
-    initialStage = 0
-    graph = [(0, ['a'], 3), (3, ['b'], 2), (3, ['c'], 2), (2, ['a'], 3), (3, ['b'], 1), (3, ['c'], 1)]
+    entrada = 0
+    if entrada == 0:  # A entrada é zero caso for uma expressão regular, utilização de q1.py e q2.py
+        word = "a*b*c*"
+        regularExpression = []
+        for symbol in word:
+            regularExpression.append(symbol)
+        graph = q1.algorithm(regularExpression)
+        initialStage = [0]
+        finalStages = [1]
+        states = []
+        for element in graph:
+            if element[0] not in states:
+                states.append(element[0])
+            if element[2] not in states:
+                states.append(element[2])
+        q2.grafo_q2(graph, states, finalStages)
+    elif entrada == 1:  # A entrada é um caso for um AFN com e-transições, utilização de q2.py
+        initialStage = [0]
+        finalStages = [1]
+        states = [0, 1, 2, 3]
+        graph = [('0','&','4'), ('4','&','2'), ('2','&','5'), ('5','&','3'), ('3','&','6'), ('6','&','1'), ('4','a','4'),
+            ('5', 'b', '5')]
+        q2.grafo_q2(graph, states, finalStages)
+    else: # A entrada é diferente de zero e um caso já for um AFN sem e-transições
+        finalStages = [0, 1, 2]
+        initialStage = 0
+        graph = [(0, ['a'], 3), (3, ['b'], 2), (3, ['c'], 2), (2, ['a'], 3), (3, ['b'], 1), (3, ['c'], 1)]
+
     for i in range(len(word1)):
         AFN(graph, initialStage, finalStages, word1[i:])
     for i in range(len(word2)):
