fe5c23c33e8df1d807163ace9b03f9786c4e9299
JoseOtavioVidal <jose.otavio.vidal@gmail.com>
2018-09-14 21:15:22 -0300

    Lab2

---
+++ Lab2/Q1/q1.py
@@ -0,0 +1,9 @@
+
+
+
+if __name__ == "__main__":
+    NFAinitialStages = []
+    NFAfinalStages = []
+    NFAgraph = []
+    DFAgraph = AFDg(AFNgraph, AFNinitialStages, AFNFinalStages)
+    AFDinitialStages = DFA
